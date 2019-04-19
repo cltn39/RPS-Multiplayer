e371b8d793e082f03f0b893b844c10cf0559a6ce
gravitation <chisong@ucdavis.edu>
2019-04-19 13:19:38 -0700

    added firebase CDN/initialization

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBVm0go9PSlCykzEZDF2y2EOAGIzTeBlOo",
+    authDomain: "rps-multiplayer-fd7e2.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-fd7e2.firebaseio.com",
+    projectId: "rps-multiplayer-fd7e2",
+    storageBucket: "rps-multiplayer-fd7e2.appspot.com",
+    messagingSenderId: "838105829531"
+  };
+  firebase.initializeApp(config);
